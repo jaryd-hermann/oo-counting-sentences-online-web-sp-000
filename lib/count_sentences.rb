558b7f5e60a5c4e48ef177c825f8eb7999b0d699
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-25 12:02:56 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -6,11 +6,13 @@ class String
     return true if self.end_with?(".")
     else
       false
-
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
   end
 
   def exclamation?
